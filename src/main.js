c7c1d6bc5c76b61003175425627b2d625061fe6d
zhaoyu hu <1755475455@qq.com>
2023-04-09 16:23:29 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {
+    components,
+    plugins
+} from './utils/elementPlus.js'
+
+const app = createApp(App)
+components.forEach(component => {
+    app.component(component.name, component)
+})
+
+plugins.forEach(plugin => {
+    app.use(plugin)
+})
+app.mount('#app')
